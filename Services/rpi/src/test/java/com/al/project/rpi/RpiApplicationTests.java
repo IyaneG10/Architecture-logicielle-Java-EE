304b5df1634775b61aa3cfe0a1deab3622f8a790
Jorge Elisio Cabral <jorge.cabraldasdores.etu>
2020-01-27 18:52:27 +0100

    Jorge

---
+++ Services/rpi/src/test/java/com/al/project/rpi/RpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al.project.rpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
