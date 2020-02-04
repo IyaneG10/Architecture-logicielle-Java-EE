e41fa43429f8e3bb640ea548cf6fc01bb030caa9
jorge.cabraldasdores.etu <jorge.cabraldasdores.etu@unive-lille.fr>
2020-02-04 08:00:36 +0100

    updatearchitecture

---
+++ app/Sysco/src/main/java/com/al/project/SysCo/YAMLConfig.java
@@ -0,0 +1,22 @@
+package com.al.project.SysCo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+
+public class YAMLConfig {
+
+    private String name;
+    private String environment;
+    private List<String> servers = new ArrayList<>();
+
+    // standard getters and setters
+
+}
