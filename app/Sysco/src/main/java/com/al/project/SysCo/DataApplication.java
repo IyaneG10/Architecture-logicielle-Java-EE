5aeb360f28e74d8ef52e5b50caba41cd037713b5
jorge.cabraldasdores.etu <jorge.cabraldasdores.etu@unive-lille.fr>
2020-02-05 16:46:41 +0100

    update

---
+++ app/Sysco/src/main/java/com/al/project/SysCo/DataApplication.java
@@ -0,0 +1,10 @@
+package com.al.project.SysCo;
+
+public class DataApplication {
+
+    public static void main(String[] args) {
+
+        //SpringApplication rpiApp = new SpringApplication(RpiApplication.class);
+        //rpiApp.run();
+    }
+}
