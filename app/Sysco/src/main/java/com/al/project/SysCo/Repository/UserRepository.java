1317e3bc3013d2cce4f442f735075909c398c17e
Mamadou malick Seck <mamadoumalick.seck.etu@univ-lille.fr>
2020-02-10 13:55:47 +0100

    Accés à ma BDD mariadb locale mais impossible de faire des traitements dessus pour le momet

--- app/Sysco/src/main/java/com/al/project/SysCo/Repository/UserRepository.java
+++ app/Sysco/src/main/java/com/al/project/SysCo/Repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.al.project.SysCo.Repository;
 
 import com.al.project.SysCo.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+
 }
--- app/Sysco/src/main/java/com/al/project/SysCo/WebApplication.java
+++ app/Sysco/src/main/java/com/al/project/SysCo/WebApplication.java
@@ -11,13 +11,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.sql.*;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
 
 public class WebApplication {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) /*throws SQLException*/ {
+/*
+        //create connection for a server installed in localhost, with a user "root" with no password
+        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/sysco", "admin", "admin")) {
+            // create a Statement
+            try (Statement stmt = conn.createStatement()) {
+                //execute query
+                try (ResultSet rs = stmt.executeQuery("SELECT 'Hello World!'")) {
+                    //position result to first
+                    rs.first();
+                    System.out.println(rs.getString(1)); //result is "Hello World!"
+                }
+            }
+        }
+*/
         SpringApplication webApp = new SpringApplication(WebApplication.class);
 
 
