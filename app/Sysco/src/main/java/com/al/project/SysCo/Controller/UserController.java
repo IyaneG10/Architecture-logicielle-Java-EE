77f09843a5710ec85c0a74a1296b2f201cd6c987
Hamza Fahim <hamza.fahim.etu@univ-lille.fr>
2020-02-26 16:09:22 +0100

    COMMIT MESSAGE

--- app/Sysco/src/main/java/com/al/project/SysCo/Controller/UserController.java
+++ app/Sysco/src/main/java/com/al/project/SysCo/Controller/UserController.java
@@ -3,12 +3,16 @@ package com.al.project.SysCo.Controller;
 import com.al.project.SysCo.Model.User;
 import com.al.project.SysCo.Service.SecurityService;
 import com.al.project.SysCo.Service.UserService;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 
 
 @Controller
@@ -45,6 +49,14 @@ public class UserController {
         return "redirect:/welcome";
     }
 
+    @PostMapping("/login/")// /room{room_id}/{measureName}
+    public String loginMobile(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) throws SQLException, JSONException {
+
+        securityService.autoLogin(username, password);
+
+        return "{\"connection\":true}";
+    }
+
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         if (error != null)
@@ -62,8 +74,8 @@ public class UserController {
     }
 
 
-        @GetMapping("/products")
-        public String list(){
+    @GetMapping("/products")
+    public String list(){
             return "products";
         }
 
