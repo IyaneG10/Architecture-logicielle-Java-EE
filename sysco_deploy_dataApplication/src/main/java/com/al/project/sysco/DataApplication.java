7c246adb7ecef83a56b41802f39a4d72c23fda49
Jorge Cabral Das Dores <jorge.cabraldasdores.etu@univ-lille.fr>
2020-03-05 13:40:06 +0100

    Added Deployment

---
+++ sysco_deploy_dataApplication/src/main/java/com/al/project/sysco/DataApplication.java
@@ -0,0 +1,71 @@
+package com.al.project.sysco;
+
+import com.al.project.sysco.Model.Data;
+import com.al.project.sysco.Service.DataService;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.charset.StandardCharsets;
+
+
+@SpringBootApplication
+public class DataApplication {
+
+    @Autowired
+    private static DataService dataService;
+
+    public static void main(String[] args)  throws Exception{
+
+        System.out.println(" START !\n To exit : 'Ctrl-C'");
+
+        final String EXCHANGE_NAME = "RPiTopics";
+        final String hostIP = "localhost";
+        final String username = "pifou";
+        final String password = "pasglop";
+        final int port = 5672;
+
+        DataService dataService = new DataService("jdbc:mariadb://localhost:3306/sysco", "admin", "admin");
+
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost(hostIP);
+        factory.setUsername(username);
+        factory.setPassword(password);
+        factory.setPort(port);
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+
+        channel.queueBind(queueName, EXCHANGE_NAME, "Rpi.DataBase.Room.*");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+
+            String jsonStringData = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [i] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + jsonStringData + "'");
+
+
+            try {
+
+                Data data= new Data(jsonStringData);
+
+                System.out.println(" [+] Data saved in database...\n");
+                dataService.addData(data);
+            }
+
+            catch (Exception ex){
+
+                System.out.println(ex);
+            }
+
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
---
+++ sysco_deploy_dataApplication/src/main/java/com/al/project/sysco/Service/DataService.java
@@ -0,0 +1,185 @@
+package com.al.project.sysco.Service;
+
+import com.al.project.sysco.Model.Data;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.*;
+
+
+public class DataService {
+
+
+    // JDBC driver name and database URL
+
+    private static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    private static String DB_URL;
+
+    //  Database credentials
+    private static String USER;
+    private static String PASSWORD;
+
+
+    java.sql.Connection conn = null;
+    static Statement stmt = null;
+
+
+    public DataService(String DB_URL, String USER, String PASSWORD) {
+        this.DB_URL = DB_URL;
+        this.USER = USER;
+        this.PASSWORD = PASSWORD;
+        configure();
+    }
+
+
+    private void  configure(){
+
+        try {
+            System.out.println(" [i] Connecting to a selected database...");
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/sysco?user=admin&password=admin");
+            stmt = ((java.sql.Connection) conn).createStatement();
+            System.out.println(" [i] Connected database successfully...\n");
+
+        }
+        catch (SQLException se) {
+
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }
+        catch (Exception e) {
+
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+    }
+
+    public void addData(Data data) throws ErrorSavingInDB, SQLException {
+
+        //STEP 4: Execute a query
+        String sql = "INSERT INTO data"
+                + "(rpi_id, state, measure_name, measure_value, date) "
+                +"VALUES "
+                + "("  + data.getRpiId()
+                + ", " + data.isState()
+                + ", '" + data.getMeasureName()
+                + "', " + data.getMeasureValue()
+                + ", '" + data.getDate()
+                + "')";
+
+        ResultSet rs = stmt.executeQuery(sql);
+    }
+
+    public JSONArray  getRealTimeData( int rpiId) throws SQLException, JSONException {
+
+        String sql = "select measure_name, state, measure_value from (select * from data where rpi_id="+ rpiId+" group by date desc ) t1 group by measure_name";
+
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        JSONArray json = new JSONArray();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while(rs.next()) {
+            int numColumns = rsmd.getColumnCount();
+            JSONObject obj = new JSONObject();
+            for (int i=1; i<=numColumns; i++) {
+                String column_name = rsmd.getColumnName(i);
+                obj.put(column_name, rs.getObject(column_name));
+
+            }
+            json.put(obj);
+        }
+        return json;
+    }
+
+
+
+    public static JSONArray getDataByAll() throws SQLException, JSONException {
+
+        //STEP 4: Execute a query
+        String sql = "SELECT * FROM data where state=true";
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        JSONArray json = new JSONArray();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while(rs.next()) {
+            int numColumns = rsmd.getColumnCount();
+            JSONObject obj = new JSONObject();
+            for (int i=1; i<=numColumns; i++) {
+                String column_name = rsmd.getColumnName(i);
+                obj.put(column_name, rs.getObject(column_name));
+            }
+            json.put(obj);
+        }
+        return json;
+    }
+
+    public  JSONArray getDataByRoom(int room) throws SQLException, JSONException {
+
+        String sql = "SELECT * FROM data WHERE rpi_id = "+ room;
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        JSONArray json = new JSONArray();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while(rs.next()) {
+            int numColumns = rsmd.getColumnCount();
+            JSONObject obj = new JSONObject();
+            for (int i=1; i<=numColumns; i++) {
+                String column_name = rsmd.getColumnName(i);
+                obj.put(column_name, rs.getObject(column_name));
+            }
+            json.put(obj);
+        }
+        return json;
+    }
+
+    public  JSONArray getDataByRoomAndSensor(int room, String measureName) throws SQLException, JSONException {
+
+        String sql = "SELECT * FROM data WHERE rpi_id = "+ room +" AND measure_name =" +"\"" +measureName+ "\"";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        JSONArray json = new JSONArray();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while(rs.next()) {
+            int numColumns = rsmd.getColumnCount();
+            JSONObject obj = new JSONObject();
+            for (int i=1; i<=numColumns; i++) {
+                String column_name = rsmd.getColumnName(i);
+                obj.put(column_name, rs.getObject(column_name));
+            }
+            json.put(obj);
+        }
+        return json;
+    }
+
+
+    public  JSONArray getRoom() throws SQLException, JSONException {
+
+        String sql = "SELECT DISTINCT rpi_id FROM data ORDER BY rpi_id ASC;";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        JSONArray json = new JSONArray();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while(rs.next()) {
+            int numColumns = rsmd.getColumnCount();
+            JSONObject obj = new JSONObject();
+            for (int i=1; i<=numColumns; i++) {
+                String column_name = rsmd.getColumnName(i);
+                obj.put(column_name, rs.getObject(column_name));
+            }
+            json.put(obj);
+        }
+        return json;
+    }
+
+
+    public class ErrorSavingInDB extends Exception {
+        public ErrorSavingInDB(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+}
+
